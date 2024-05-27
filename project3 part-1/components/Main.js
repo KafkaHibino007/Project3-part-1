8656d9aaaac2a123484c123996a2e07596da596f
Kafka Hibino <babyabh@gmail.com>
2024-05-27 13:39:34 +0530

    Add files via upload

---
+++ project3 part-1/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Main() {
+    return (
+    <section>
+    <img src = "./images/imagegrid.png" />
+    <h1> Online Experiences </h1>
+    <h2> Unique interactive activities led by 
+        one-of-a-kind hosts- all without leaving home.</h2>
+         
+    </section>
+    )
+}
